5c8163d3d4335ac00ffbd90af7819830b5a56146
HarryYin03 <u6511238@au.edu>
2024-07-16 21:02:54 +0700

    my first react page

---
+++ React app/src/Food.jsx
@@ -0,0 +1,14 @@
+function Food(){
+    const food1 = "Pizza";
+    const food2 = "BaBa Shit"
+
+    return(
+        <ul>
+            <li>BaBa saliva</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    )
+}
+
+export default Food;
---
+++ React app/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()} This is BaBa Website</p>
+        </footer>
+    )
+}
+
+export default Footer;
